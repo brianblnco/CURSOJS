3b5a100710ac18980192bfc13163145db184d1a8
BRIAN FERNANDEZ BLANCO <0017037@viewnext.com>
2022-10-06 18:56:31 +0200

    v.0

---
+++ prueba1/app.js
@@ -0,0 +1,13 @@
+console.log("FUNCIONA EL JS");
+
+var numeros = [];
+console.log(numeros);
+
+
+
+for (let i = 0; i < 10; i++) {
+    numeros[i] = i+1;
+    console.log("La posición " + i + " del array contiene el valor: " + numeros[i]);
+    
+}
+
